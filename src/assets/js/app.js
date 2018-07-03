5ae14c8b5459b7e72c67823334b87870b286c55c
nerdale <alexandra.neira@gmail.com>
2018-07-03 02:41:34 -0400

    ok login google y database post con firebase

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,7 @@
 (function () {
   function init() {
     var router = new Router([
-      new Route('index', '../index.html'),
+      new Route('index', '../../index.html'),
       new Route('profile', '../static/views/profile.html')
     ]);
   }
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,8 @@
+const container = document.getElementById('root');
+const containerLogin = document.getElementById('log');
+
 window.onload = () => {
+	// inicializo firebase
 	var config = {
     apiKey: "AIzaSyB5VGUJtLOPuyrYmVKXNOO9JWw5sNhRX6k",
     authDomain: "socialnetwork-79af6.firebaseapp.com",
@@ -8,46 +12,77 @@ window.onload = () => {
     messagingSenderId: "723320283252"
   };
 	firebase.initializeApp(config);
-}
 
-const googleSignIn = () => {
-	base_provider = new firebase.auth.GoogleAuthProvider();
-	firebase.auth().signInWithPopup(base_provider).then(result => {
-		console.log(result);
-		console.log('Succes... Google Account Linked');
-		showComment(result);
-		
-		
-	}).catch(err => {
-		console.log(err);
-		console.log('Failed to do');
+	// iniciar sesión con google firebase al clickear botón
+	document.getElementById('botonlogin').addEventListener('click', (event) => { 
+		let base_provider = new firebase.auth.GoogleAuthProvider();
+		base_provider.addScope('email');
+		firebase.auth().signInWithPopup(base_provider)
+		.then(result => {
+			console.log(result);
+			console.log('Succes... Google Account Linked ', result.additionalUserInfo.profile.name);
+		}).catch(err => {
+			console.log(err);
+			console.log('Failed to do ', err);
+		})
 	})
+	
+	// cerrar sesión con google firebase al clickear botón
+	document.getElementById('botonlogout').addEventListener('click', (event) => {
+		firebase.auth().signOut();
+		container.innerHTML = ''; // se limpia div con los comentarios
+		containerLogin.style.display = 'block'; // re aparece botón login
+	});
+	
+	// verifica si la sesión está iniciada
+	firebase.auth().onAuthStateChanged((user) => {
+		if (user) {
+			//console.log('AuthStateChanged', user);
+			document.getElementById('botonlogin').style.display = 'none'
+			document.getElementById('botonlogout').style.display = 'block';
+			showComment(user);
+			add();
+			show();
+		} else {
+			document.getElementById('botonlogin').style.display = 'block'
+			document.getElementById('botonlogout').style.display = 'none';
+			//swal('Hey Psssss', '...you must log in');
+		}
+	});
 }
 
-
-const showComment = (result) => {
-	const container = document.getElementById('root');
-	document.getElementById('log').style.display = 'none';
-	container.innerHTML = `	<section><h2>Hola ${result.additionalUserInfo.profile.name}</h2><div class="container-fluid"><div id="contenedor"></div><div class="row"><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><div class="form-group text-center"><textarea class="txt" id="comment" placeholder="Comment..."></textarea><button type="button" class="btn btn-primary pull-right" id="btn-add" onclick="add()"><i class="fas fa-plus"></i> Add comment</button></div></div></div></div></section>`
-}
+// mostrar textarea para agregar comentario
+const showComment = (user) => {
+	console.log(user.displayName)
+	containerLogin.style.display = 'none';
+	container.innerHTML += `<section><h2>${user.displayName}</h2><div class="container-fluid"><div id="contenedor">}</div><div class="row"><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><div class="form-group text-center"><textarea class="txt" id="comment" placeholder="Comment..."></textarea><button type="button" class="btn btn-primary pull-right" id="btn-add" onclick="add()"><i class="fas fa-plus"></i> Add comment</button></div></div></div></div></section>`
+};
 
 const add = (event) => {
-	const cont = document.getElementById('contenedor');
-	// referencia al nodo raiz de la base de datos
-	const databaseRef = firebase.database().ref().child('comentarios');	
+	// muestra los comentarios guardados en firebase
+	show();
+	if (document.getElementById('comment').value.length === 0) {
+		console.log('mensaje vacio')
+		return;
+	}
+	// referencia al nodo raiz de la base de datos (comentarios) para crearle hijos
+	const databaseRef = firebase.database().ref().child('comentarios');
+	// agrega comentarios a database firebase
 	databaseRef.push({
 		comment: document.getElementById('comment').value
-		//event.target.comment.value
-	})
-
-	let comment = document.getElementById('comment').value;
+	});	
 	document.getElementById('comment').value = '';
-	
-	if (comment === null || comment.length === 0) {
-		swal('Hey Psssss', '...where is the comment?');
-		return false;
-	}
+};
 
-	cont.innerHTML += `<div class="card"><div class="row"><div class="col-xs-3"><a href="#profile" title="Profile"><img src="static/img/grumpy.jpg" class="img-circle img-user" alt="User"></a></div><div class="col-xs-9"><div class="comment"><h5><a href="#profile" title="Profile">Grumpy:</a></h5><p>${comment}</p><div><i class="fas fa-heart"></i></div></div></div></div></div>`
-
-}
+const show = () => {
+	const containerComment = document.getElementById('contenedor');
+	firebase.database().ref().child('comentarios').on('value', (snap) => {
+		var datos = snap.val();
+		let result = '';
+		console.log(datos)
+		for (var key in datos) {
+			result += `<div class="card"><div class="row"><div class="col-xs-3"><a href="#profile" title="Profile"><img src="static/img/grumpy.jpg" class="img-circle img-user" alt="User"></a></div><div class="col-xs-9"><div class="comment"><h5><a href="#profile" title="Profile">Grumpy:</a></h5><p>${datos[key].comment}</p><div><i class="fas fa-heart"></i></div></div></div></div></div>`
+		}
+		containerComment.innerHTML = result;
+	})
+};
